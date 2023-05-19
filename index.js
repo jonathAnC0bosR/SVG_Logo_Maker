0de83c35a859e7cb35146e468235a93388948c80
Jonathan Cobos <jonathan.cobos@outlook.es>
2023-05-19 17:22:06 -0600

    Added code that renders all figures dinamically

--- index.js
+++ index.js
@@ -38,25 +38,38 @@ const render = function(data) {
     if(data.shape === 'circle') {
         
         const newCircle = new Circle(data.text, data.textColor, data.shapesColor, data.shape);
-        console.log('This is a Circle! ' , newCircle);
         const fileName = 'newCircleEx.svg';
-        const filePath = path.join(folderPath, fileName);
-        const svgCircle = generateSvgCircle(newCircle);
+        let filePath = path.join(folderPath, fileName);
+        const svgCircle = renderCircle(newCircle);
         fs.writeFile(filePath, svgCircle, (err) => {
-            err ? console.log(err) : console.log('File Created');
+            err ? console.log(err) : console.log('File created succesfully! ');
         })
 
         
     } else if (data.shape === 'triangle') {
+        
         const newTriangle = new Triangle(data.text, data.textColor, data.shapesColor, data.shape);
-        console.log('This is a Triangle! ' , newTriangle);
+        const fileName = 'newTriangle.svg';
+        let filePath = path.join(folderPath, fileName);
+        const svgTriangle = renderTriangle(newTriangle);
+        fs.writeFile(filePath, svgTriangle, (err) => {
+            err ? console.log(err) : console.log('File created succesfully! ');
+        })
+        
     } else  {
+        
         const newSquare = new Square(data.text, data.textColor, data.shapesColor, data.shape);
-        console.log('This is a square!');
+        const fileName = 'newSquare.svg';
+        let filePath = path.join(folderPath, fileName);
+        const svgSquare = renderSquare(newSquare);
+        fs.writeFile(filePath, svgSquare, (err) => {
+            err ? console.log(err) : console.log('File created succesfully! '); 
+        })
+
     }
 }
 
-const generateSvgCircle = ({text, textColor, shapesColor, shape}) => 
+const renderCircle = ({text, textColor, shapesColor, shape}) => 
 `<svg version="1.1"
 width="300" height="200"
 xmlns="http://www.w3.org/2000/svg">
@@ -66,4 +79,25 @@ xmlns="http://www.w3.org/2000/svg">
 <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
 
 </svg>`
+;
+
+const renderTriangle = ({text, textColor, shapesColor, shape}) =>
+`<svg version="1.1" 
+width="300" height="200"
+xmlns="http://www.w3.org/2000/svg">
+
+<polygon points="150,40 40,180 260,180" font-size="30" text-anchor = "middle" fill="${shapesColor}" />
+<text x="150" y="145" font-size="40" text-anchor="middle" fill="${textColor}">${text}</text>
+</svg>`
+;
+
+const renderSquare = ({text, textColor, shapesColor, shape}) =>
+`<svg version="1.1" 
+width="300" height="200" 
+xmlns="http://www.w3.org/2000/svg">
+
+<rect x="50" y="50" width="200" height="100" fill="${shapesColor}" />
+<text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
+</svg>
+`
 ;
