efaa5348a171fbd77ea119ee026c3cf24d295ac0
Jonathan Cobos <jonathan.cobos@outlook.es>
2023-05-19 16:46:12 -0600

    Added code that creates the svg files and places it inside the examples folder

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const {Circle} = require('./lib/shapes');
+const {Circle, Triangle, Square} = require('./lib/shapes');
+const path = require('path');
+const folderPath = './examples';
+
 
 inquirer
     .prompt([
@@ -18,7 +21,7 @@ inquirer
             type: 'list',
             message: 'Select a shape:',
             name: 'shape',
-            choices: ['Circle', 'Triangle', 'Square'],
+            choices: ['circle', 'triangle', 'square'],
         },
         {
             type: 'input',
@@ -27,16 +30,40 @@ inquirer
         },
     ])
     .then((data) => {
-        // const userInput = {
-        //     text: data.text,
-        //     textColor: data.textColor,
-        //     shapesColor: data.shapesColor,
-        //     shape: data.shape,
-        // }
-
-
-        // console.log(userInput);
-        const newShape = new Circle(data.text, data.textColor, data.shapesColor, data.shape);
-        console.log(newShape);
+        render(data);
     })
 
+
+const render = function(data) {
+    if(data.shape === 'circle') {
+        
+        const newCircle = new Circle(data.text, data.textColor, data.shapesColor, data.shape);
+        console.log('This is a Circle! ' , newCircle);
+        const fileName = 'newCircleEx.svg';
+        const filePath = path.join(folderPath, fileName);
+        const svgCircle = generateSvgCircle(newCircle);
+        fs.writeFile(filePath, svgCircle, (err) => {
+            err ? console.log(err) : console.log('File Created');
+        })
+
+        
+    } else if (data.shape === 'triangle') {
+        const newTriangle = new Triangle(data.text, data.textColor, data.shapesColor, data.shape);
+        console.log('This is a Triangle! ' , newTriangle);
+    } else  {
+        const newSquare = new Square(data.text, data.textColor, data.shapesColor, data.shape);
+        console.log('This is a square!');
+    }
+}
+
+const generateSvgCircle = ({text, textColor, shapesColor, shape}) => 
+`<svg version="1.1"
+width="300" height="200"
+xmlns="http://www.w3.org/2000/svg">
+
+<${shape} cx="150" cy="100" r="80" fill="${shapesColor}" />
+
+<text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
+
+</svg>`
+;
